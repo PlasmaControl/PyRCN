1dca930af38ed7d9c7f20169fb8ece463539a3aa
renierts <peter.steiner@tu-dresden.de>
2020-07-16 17:24:45 +0200

    re-structured to created a package in the future.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PyRCN
 A Python 3 framework for Reservoir Computing with a scikit-learn-compatible API.
+
+
+# Acknowledgements
+This research is financed by Europäischer Sozialfonds (ESF), the Free State of Saxony and Ghent University.
+![SMWA_EFRE-ESF Logo](https://github.com/TUD-STKS/PyRCN/images/SMWA_EFRE-ESF_Sachsen_Logokombi_quer_03.jpg)
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrcn",
+    version="0.0.2",
+    author="Peter Steiner",
+    author_email="peter.steiner@tu-dresden.de",
+    description="A Python3 framework for Reservoir Computing with a scikit-learn-compatible API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TUD-STKS/PyRCN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+    ],
+    keywords='PyRCN, Echo State Network',
+    project_urls={
+        'Documentation': 'https://github.com/TUD-STKS/PyRCN',
+        'Funding': 'https://github.com/TUD-STKS/PyRCN',
+        'Source': 'https://github.com/TUD-STKS/PyRCN',
+        'Tracker': 'https://github.com/TUD-STKS/PyRCN/issues',
+    },
+    install_requires=[
+        'scikit-learn>=0.22.1',
+        'numpy>=1.18.1',
+        'scipy>=1.2.0',
+        'joblib>=0.13.2',
+    ],
+    python_requires='>=3.6',
+)
