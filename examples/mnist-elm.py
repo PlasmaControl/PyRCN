8e41d77132dee64de255cbb90a86620344e560de
michael <m.schindler94@googlemail.com>
2021-03-17 02:35:36 +0100

    # experiment adaption

--- examples/mnist-elm.py
+++ examples/mnist-elm.py
@@ -1021,7 +1021,6 @@ def silhouette_n_clusters(directory, *args, **kwargs):
 
     # n_clusters
     k = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 200, 500, 1000, 2000, 4000]
-    k = [200]
 
     # n_init
     n_init = 10
