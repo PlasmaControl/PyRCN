7c7c5bdc84f321051894f1b5f775d67e6e9ce74c
renierts <peter.steiner@tu-dresden.de>
2021-04-09 11:06:53 +0200

    Possibly bugfix in base.py

--- pyrcn/base.py
+++ pyrcn/base.py
@@ -611,7 +611,7 @@ class NodeToNode(BaseEstimator, TransformerMixin):
         self
         """
         self._validate_hyperparameters()
-        self._validate_data(X, y, multi_output=True)
+        self._validate_data(X=X, y=y, multi_output=True)
         self._check_n_features(X, reset=True)
 
         if self.k_rec is not None:
