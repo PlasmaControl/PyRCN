94511b2e7ce5e39376e189ba377f6962ec8d904f
renierts <peter.steiner@tu-dresden.de>
2021-04-17 10:55:29 +0200

    Ensure that SequentialSearch() can also deal with one step passed without list

--- pyrcn/model_selection/_search.py
+++ pyrcn/model_selection/_search.py
@@ -2,7 +2,7 @@
 SearchCV pipeline for incremental hyper-parameter search
 """
 
-# Authors: Simon Stone <simon.stone@tu-dresden.de>
+# Authors: Simon Stone <simon.stone@tu-dresden.de>, Peter Steiner <peter.steiner@tu-dresden.de>
 # License: BSD 3 clause
 
 
@@ -18,22 +18,39 @@ class SequentialSearch(BaseSearchCV):
                  pre_dispatch='2*n_jobs', error_score=np.nan, return_train_score=True):
         super().__init__(estimator, scoring=scoring, n_jobs=n_jobs, refit=refit, cv=cv, verbose=verbose,
                          pre_dispatch=pre_dispatch, error_score=error_score, return_train_score=return_train_score)
-        self.searches = searches
+        if isinstance(searches, list):
+            self.searches = searches
+        else:
+            self.searches = list(searches)
         self.best_estimator_ = None
+        self.results = {}
 
     def fit(self, X, y=None, *, groups=None, **fit_params):
         """
-        TODO
-        :param groups:
-        :param X:
-        :param y:
-        :return:
+        Run fit with all sets of parameters.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training vector, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like of shape (n_samples, n_output) \
+            or (n_samples,), default=None
+            Target relative to X for classification or regression;
+            None for unsupervised learning.
+        groups : array-like of shape (n_samples,), default=None
+            Group labels for the samples used while splitting the dataset into
+            train/test set. Only used in conjunction with a "Group" :term:`cv`
+            instance (e.g., :class:`~sklearn.model_selection.GroupKFold`).
+        **fit_params : dict of str -> object
+            Parameters passed to the ``fit`` method of the estimator
         """
-        for name, search, params, *kwargs in self.searches:
+        for step, name, search, params, *kwargs in enumerate(self.searches):
             if len(kwargs) == 1:
-                result = search(self.estimator, params, refit=True, **kwargs[0]).fit(X, y)
+                result = search(self.estimator, params, refit=True, **kwargs[0]).fit(X, y, groups, **fit_params)
             else:
                 result = search(self.estimator, params, refit=True).fit(X, y)
+                self.results['step'+str(step)] = cv_results_
                 # TODO
                 """
                 Add all attributes of the search result object as a dictionary to a results dictionary:
