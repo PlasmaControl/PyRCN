8c45733c76b40c9eb250004f41ea4d761087c3d2
renierts <peter.steiner@tu-dresden.de>
2021-04-20 15:35:18 +0200

    switching to HPC for ESN MNIST experiments.

--- examples/mnist_sequential.py
+++ examples/mnist_sequential.py
@@ -70,47 +70,16 @@ step3_esn_params = {'node_to_node__bias_scaling': np.linspace(0.0, 1.5, 16)}
 
 base_esn = ESNClassifier(input_to_node=InputToNode(), node_to_node=NodeToNode(), regressor=IncrementalRegression()).set_params(**initially_fixed_params)
 
-acc_scores = Parallel(n_jobs=2, verbose=10)(delayed(optimize_esn)(base_esn, params, X_train, y_train) for params in ParameterGrid(step1_esn_params))
+acc_scores = Parallel(n_jobs=-1, verbose=10)(delayed(optimize_esn)(base_esn, params, X_train, y_train) for params in ParameterGrid(step1_esn_params))
 base_esn.set_params(**ParameterGrid(step1_esn_params)[np.argmax(acc_scores)])
 final_fixed_params.update(ParameterGrid(step1_esn_params)[np.argmax(acc_scores)])
 
-acc_scores = Parallel(n_jobs=2, verbose=10)(delayed(optimize_esn)(base_esn, params, X_train, y_train) for params in ParameterGrid(step2_esn_params))
+acc_scores = Parallel(n_jobs=-1, verbose=10)(delayed(optimize_esn)(base_esn, params, X_train, y_train) for params in ParameterGrid(step2_esn_params))
 base_esn.set_params(**ParameterGrid(step1_esn_params)[np.argmax(acc_scores)])
 final_fixed_params.update(ParameterGrid(step2_esn_params)[np.argmax(acc_scores)])
 
-acc_scores = Parallel(n_jobs=2, verbose=10)(delayed(optimize_esn)(base_esn, params, X_train, y_train) for params in ParameterGrid(step3_esn_params))
+acc_scores = Parallel(n_jobs=-1, verbose=10)(delayed(optimize_esn)(base_esn, params, X_train, y_train) for params in ParameterGrid(step3_esn_params))
 base_esn.set_params(**ParameterGrid(step1_esn_params)[np.argmax(acc_scores)])
 final_fixed_params.update(ParameterGrid(step3_esn_params)[np.argmax(acc_scores)])
 
-base_elm_ridge = ESNClassifier(input_to_node=InputToNode(), regressor=Ridge()).set_params(**final_fixed_params)
-base_elm_inc = ESNClassifier(input_to_node=InputToNode(), regressor=IncrementalRegression()).set_params(**final_fixed_params)
-base_elm_inc_chunk = clone(base_elm_inc).set_params(chunk_size=6000)
-
-param_grid = {'input_to_node__hidden_layer_size': [16000]}
-
-print("Estimator\tFit time\tInference time\tAccuracy score\tSize[Bytes]")
-for params in ParameterGrid(param_grid):
-    t1 = time.time()
-    elm_ridge = clone(base_elm_ridge).set_params(**params).fit(X_train, y_train)
-    t_fit = time.time() - t1
-    mem_size = elm_ridge.__sizeof__()
-    t1 = time.time()
-    acc_score = accuracy_score(y_test, elm_ridge.predict(X_test))
-    t_inference = time.time() - t1
-    print("{0}\t{1}\t{2}\t{3}\t{4}".format(elm_ridge, t_fit, t_inference, acc_score, mem_size))
-    t1 = time.time()
-    elm_inc = clone(base_elm_inc).set_params(**params).fit(X_train, y_train)
-    t_fit = time.time() - t1
-    mem_size = elm_inc.__sizeof__()
-    t1 = time.time()
-    acc_score = accuracy_score(y_test, elm_inc.predict(X_test))
-    t_inference = time.time() - t1
-    print("{0}\t{1}\t{2}\t{3}\t{4}".format(elm_inc, t_fit, t_inference, acc_score, mem_size))
-    t1 = time.time()
-    elm_inc_chunk = clone(base_elm_inc_chunk).set_params(**params).fit(X_train, y_train)
-    t_fit = time.time() - t1
-    mem_size = elm_inc_chunk.__sizeof__()
-    t1 = time.time()
-    acc_score = accuracy_score(y_test, elm_inc_chunk.predict(X_test))
-    t_inference = time.time() - t1
-    print("{0}\t{1}\t{2}\t{3}\t{4}".format(elm_inc_chunk, t_fit, t_inference, acc_score, mem_size))
+print(final_fixed_params)
