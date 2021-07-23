09f24f2849f8b7f8d5ff98bfd79e7839752043c9
renierts <peter.steiner@tu-dresden.de>
2021-07-23 17:53:06 +0200

    Video experiments are almost finished.

--- examples/Video_Door_state_Classification.py
+++ examples/Video_Door_state_Classification.py
@@ -3,14 +3,16 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from pyrcn.base import InputToNode, NodeToNode
+from pyrcn.base import InputToNode, PredefinedWeightsInputToNode, NodeToNode
 from pyrcn.linear_model import IncrementalRegression
 from pyrcn.echo_state_network import SeqToSeqESNClassifier
-from pyrcn.metrics import accuracy_score, classification_report, confusion_matrix
+from pyrcn.metrics import accuracy_score, classification_report, confusion_matrix, mean_squared_error
 from pyrcn.model_selection import SequentialSearchCV
 
-from sklearn.metrics import make_scorer
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.base import clone
+from sklearn.metrics import make_scorer, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, ParameterGrid
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.utils.fixes import loguniform
 from scipy.stats import uniform
 from joblib import dump, load
@@ -24,7 +26,7 @@ def read_file(fname,Nfr=-1):
     N_fr=dim[0]*dim[1] # size of the input vector
     yuvfile= open(fname+'.yuv', 'rb') # Opening the video file
     door_state_file= open(fname+'.txt', 'rb') # Opening the annotation file
-    TARGET=np.zeros((T, ))
+    TARGET=np.zeros((T, 3))
     FRAMES=np.zeros((T,N_fr))
     for t in tqdm(range(T)): # for each frame    
         fr2=np.zeros(N_fr) 
@@ -34,12 +36,12 @@ def read_file(fname,Nfr=-1):
         # ----------------------------------    
         fr2=fr2/255.0 # Normalizing the pixel values to [0,1]
         FRAMES[t,:]=fr2
-        TARGET[t] = int(door_state_file.read(1))
+        TARGET[t,int(door_state_file.read(1))] = 1 # setting the desired output class to 1
     return FRAMES, TARGET
 
 
 try:
-    X_train, X_test, y_train, y_test = load("Seq_video_dataset.joblib")
+    X_train, X_test, y_train, y_test = load(r"E:\RCN_CICSyN2015\Seq_video_dataset_large.joblib")
 except FileNotFoundError:
     n_files = 3
 
@@ -47,7 +49,7 @@ except FileNotFoundError:
     y_total = [None] * n_files
     n_sequences_total = [None] * n_files
     for k in range(n_files):
-        X_total[k], y_total[k] = read_file("projects/p_transcriber/RCN_CICSyN2015/Seq_" + str(k + 1))
+        X_total[k], y_total[k] = read_file(r"E:\RCN_CICSyN2015\Seq_" + str(k + 1))
 
     X_train_list = []
     y_train_list = []
@@ -55,7 +57,7 @@ except FileNotFoundError:
     y_test_list = []
 
     for k in range(n_files):
-        n_sequences_total[k] = int(len(X_total[k]) / 1800)
+        n_sequences_total[k] = int(len(X_total[k]) / 5400)
         X_total[k] = np.array_split(X_total[k], n_sequences_total[k])
         y_total[k] = np.array_split(y_total[k], n_sequences_total[k])
         for m, (X, y) in enumerate(zip(X_total[k], y_total[k])):
@@ -73,16 +75,20 @@ except FileNotFoundError:
 
     for k, (X, y) in enumerate(zip(X_train_list, y_train_list)):
         X_train[k] = X.astype(float)
-        y_train[k] = y.astype(float)
+        y_train[k] = y.astype(int)
 
     for k, (X, y) in enumerate(zip(X_test_list, y_test_list)):
         X_test[k] = X.astype(float)
-        y_test[k] = y.astype(float)
+        y_test[k] = y.astype(int)
     
-    dump([X_train, X_test, y_train, y_test], "Seq_video_dataset.joblib")
+    dump([X_train, X_test, y_train, y_test], r"E:\RCN_CICSyN2015\Seq_video_dataset_large.joblib")
 
 print(X_train.shape, X_train[0].shape, y_train.shape, y_train[0].shape)
 print(X_test.shape, X_test[0].shape, y_test.shape, y_test[0].shape)
+
+kmeans = MiniBatchKMeans(n_clusters=50, n_init=200, reassignment_ratio=0, max_no_improvement=50, init='k-means++', verbose=2, random_state=0)
+kmeans.fit(X=np.concatenate(np.concatenate((X_train, X_test))))
+w_in = np.divide(kmeans.cluster_centers_, np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
 
 initially_fixed_params = {'hidden_layer_size': 50,
                           'k_in': 10,
@@ -117,10 +123,23 @@ searches = [('step1', RandomizedSearchCV, step1_esn_params, kwargs_step1),
             ('step3', GridSearchCV, step3_esn_params, kwargs_step3),
             ('step4', RandomizedSearchCV, step4_esn_params, kwargs_step4)]
 
-base_esn = SeqToSeqESNClassifier(**initially_fixed_params)
+base_km_esn = SeqToSeqESNClassifier(input_to_node=PredefinedWeightsInputToNode(predefined_input_weights=w_in.T),
+                                    **initially_fixed_params)
 
 try:
-    sequential_search = load("sequential_search_RICSyN2015_large.joblib")
+    sequential_search = load("../sequential_search_RICSyN2015_km_large.joblib")
 except FileNotFoundError:
-    sequential_search = SequentialSearchCV(base_esn, searches=searches).fit(X_train, y_train)
-    dump(sequential_search, "sequential_search_RICSyN2015_large.joblib")
+    sequential_search = SequentialSearchCV(base_km_esn, searches=searches).fit(X_train, y_train)
+    dump(sequential_search, "../sequential_search_RICSyN2015_km_large.joblib")
+
+base_esn = clone(sequential_search.best_estimator_)
+
+param_grid = {'hidden_layer_size': [50, 100, 200, 400, 800, 1600],
+              'random_state': range(1, 11)}
+
+for params in ParameterGrid(param_grid):
+    esn = clone(base_esn).set_params(**params).fit(X=X_train, y=y_train, n_jobs=8)
+    y_pred = esn.predict_proba(X_test)
+    score = accuracy_score(np.argmax(np.concatenate(y_test), axis=1), np.argmax(np.concatenate(y_pred), axis=1))
+    print("ESN with params {0} achieved score of {1}".format(params, score))
+
