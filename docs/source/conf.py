f1ca4386fd643932cb7af9844a014ddba3ae45d8
renierts <peter.steiner@tu-dresden.de>
2021-10-26 13:44:03 +0200

    changed the master doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx_copybutton'
 ]
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
