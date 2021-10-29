8f3eb509d1cbd70731d6581b33aa8fb698835ebd
renierts <peter.steiner@tu-dresden.de>
2021-10-29 16:27:17 +0200

    Removed FeedbackESN for now, because it needs a lot of refactoring.
    Removed SeqTo* models, because they are no more required.

--- pyrcn/echo_state_network/__init__.py
+++ pyrcn/echo_state_network/__init__.py
@@ -20,14 +20,7 @@ References
 # License: BSD 3 clause
 
 from pyrcn.echo_state_network._esn import ESNClassifier, ESNRegressor
-from pyrcn.echo_state_network._feedback_esn import FeedbackESNRegressor
-from pyrcn.echo_state_network._sequence_to_sequence_model import SeqToSeqESNRegressor, SeqToSeqESNClassifier
-from pyrcn.echo_state_network._sequence_to_label_model import SeqToLabelESNClassifier
 
 __all__ = ['ESNClassifier',
            'ESNRegressor',
-           'FeedbackESNRegressor',
-           'SeqToSeqESNRegressor',
-           'SeqToSeqESNClassifier',
-           'SeqToLabelESNClassifier',
            ]
