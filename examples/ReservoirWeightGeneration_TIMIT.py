a580b11b6167d5b56053ecc9f1301af3e2a4211a
renierts <peter.steiner@tu-dresden.de>
2021-11-15 10:24:53 +0100

    Updated TIMIT experiment.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -212,15 +212,16 @@ searches = [('step1', RandomizedSearchCV, step1_esn_params, kwargs_step1),
             ('step3', RandomizedSearchCV, step3_esn_params, kwargs_step3),
             ('step4', RandomizedSearchCV, step4_esn_params, kwargs_step4)]
 
-base_esn = ESNClassifier().set_params(**initially_fixed_params)
+base_esn = ESNClassifier(
+    input_to_node=input_to_node).set_params(**initially_fixed_params)
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_random.joblib")
+        "../sequential_search_speech_timit_kmeans.joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(base_esn,
                                            searches=searches).fit(X_train,
                                                                   y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_random.joblib")
+         "../sequential_search_speech_timit_kmeans.joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
