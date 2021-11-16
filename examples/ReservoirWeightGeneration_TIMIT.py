b4f6fc703986523ee1b42a1b90aa05ffe5cf2810
renierts <peter.steiner@tu-dresden.de>
2021-11-16 06:42:38 +0100

    Other normalization method

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -159,11 +159,10 @@ for k, X in enumerate(X_test):
 kmeans = load("../kmeans_50.joblib")
 w_in = np.divide(kmeans.cluster_centers_,
                  np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
-w_bias = 2*(np.unique(kmeans.labels_, return_counts=True)[1]
-            /len(kmeans.labels_)) - 1
+w_bias = np.unique(kmeans.labels_, return_counts=True)[1] / len(kmeans.labels_)
 input_to_node = PredefinedWeightsInputToNode(predefined_input_weights=w_in.T,
                                              predefined_bias_weights=w_bias)
-w_rec = 2*transition_matrix(kmeans.labels_) - 1
+w_rec = transition_matrix(kmeans.labels_)
 node_to_node = PredefinedWeightsNodeToNode(predefined_recurrent_weights=w_rec)
 
 initially_fixed_params = {
@@ -219,11 +218,11 @@ base_esn = ESNClassifier(input_to_node=input_to_node,
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_kmeans_bias_rec_-1_1.joblib")
+        "../sequential_search_speech_timit_kmeans_bias_rec_0_1.joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(base_esn,
                                            searches=searches).fit(X_train,
                                                                   y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_kmeans_bias_rec_-1_1.joblib")
+         "../sequential_search_speech_timit_kmeans_bias_rec_0_1.joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
