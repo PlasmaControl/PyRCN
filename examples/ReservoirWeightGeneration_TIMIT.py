73e7ef68dae00f462bdfc4741b84c1123a776eba
renierts <peter.steiner@tu-dresden.de>
2021-11-26 09:58:12 +0100

    KM-ESN attention experiment on TIMIT for the last time (hopefully).

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -160,8 +160,7 @@ for k, X in enumerate(X_test):
 kmeans = load("../kmeans_50.joblib")
 w_in = np.divide(kmeans.cluster_centers_,
                  np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
-w_bias = 2 * (np.unique(kmeans.labels_, return_counts=True)[1]
-              / len(kmeans.labels_)) - 1
+w_bias = np.unique(kmeans.labels_, return_counts=True)[1] / len(kmeans.labels_)
 input_to_node = PredefinedWeightsInputToNode(
     predefined_input_weights=w_in.T,
     predefined_bias_weights=w_bias
@@ -221,10 +220,10 @@ base_esn = ESNClassifier(
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_km_esn_attention.joblib")
+        "../sequential_search_speech_timit_km_esn_attention_0_1.joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(
         base_esn, searches=searches).fit(X_train, y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_km_esn_attention.joblib")
+         "../sequential_search_speech_timit_km_esn_attention_0_1.joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
