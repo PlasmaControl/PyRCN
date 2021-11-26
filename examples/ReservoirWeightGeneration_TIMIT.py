58b6c74918f7fe7f13242ff732ef2f866ac07ad5
renierts <peter.steiner@tu-dresden.de>
2021-11-26 12:14:37 +0100

    KM-ESN recurrent experiment on TIMIT for the last time (hopefully).

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -165,10 +165,8 @@ input_to_node = PredefinedWeightsInputToNode(
     predefined_input_weights=w_in.T,
     predefined_bias_weights=w_bias
 )
-w_rec = 2 * transition_matrix(kmeans.labels_) - 1
-we = np.linalg.eigvals(w_rec)
-w_rec = w_rec / np.amax(np.absolute(we))
-node_to_node = PredefinedWeightsNodeToNode(predefined_recurrent_weights=w_rec)
+w_rec = transition_matrix(kmeans.labels_)
+node_to_node = AttentionWeightsNodeToNode(recurrent_attention_weights=w_rec)
 
 initially_fixed_params = {
     'hidden_layer_size': 50,
@@ -223,12 +221,12 @@ base_esn = ESNClassifier(input_to_node=input_to_node,
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_km_esn_attention_rec_eig_-1_1"
+        "../sequential_search_speech_timit_km_esn_attention_rec_attention"
         ".joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(
         base_esn, searches=searches).fit(X_train, y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_km_esn_attention_rec_eig_-1_1"
+         "../sequential_search_speech_timit_km_esn_attention_rec_attention"
          ".joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
