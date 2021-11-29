3a10c9f8fea9b6e9c688f3041c93ad81a47560b9
renierts <peter.steiner@tu-dresden.de>
2021-11-29 14:54:05 +0100

    Partially increase the hidden_layer_size of the base ESN.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -229,10 +229,10 @@ except FileNotFoundError:
          "../sequential_search_basic_esn.joblib")
 
 param_grid = {
-    'hidden_layer_size': [50, 100, 200, 400, 500, 800, 1000],
+    'hidden_layer_size': [1600, 2000, 3200, 4000, 8000],  # , 16000
 }
 gs = GridSearchCV(
     clone(sequential_search.best_estimator_), param_grid, scoring=scoring,
     n_jobs=-1, refit=False, verbose=10).fit(X_train, y_train)
-dump(gs, "../sequential_search_basic_esn_final_1.joblib")
+dump(gs, "../sequential_search_basic_esn_final_2.joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
