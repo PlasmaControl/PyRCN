6f2d74545fe876e129950d408b71643ea3ebe6be
renierts <peter.steiner@tu-dresden.de>
2021-11-29 14:38:48 +0100

    Increase the hidden_layer_size of the base ESN.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -10,7 +10,8 @@ from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.utils import shuffle
 from sklearn.utils.fixes import loguniform
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.base import clone
+from sklearn.model_selection import (RandomizedSearchCV, GridSearchCV)
 from sklearn.metrics import make_scorer
 from pyrcn.metrics import accuracy_score
 from pyrcn.model_selection import SequentialSearchCV
@@ -220,10 +221,19 @@ base_esn = ESNClassifier().set_params(**initially_fixed_params)
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_basic_esn.joblib")
+        "../sequential_search_basic_esn.joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(
         base_esn, searches=searches).fit(X_train, y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_basic_esn.joblib")
+         "../sequential_search_basic_esn.joblib")
+
+param_grid = {
+    'hidden_layer_size': [50, 100, 200, 400, 500, 800, 1000,
+                          1600, 2000, 3200, 4000, 8000, 16000],
+}
+gs = GridSearchCV(
+    clone(sequential_search.best_estimator_), param_grid, scoring=scoring,
+    n_jobs=-1, refit=False, verbose=10).fit(X_train, y_train)
+dump(gs, "../sequential_search_basic_esn_final_1.joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
