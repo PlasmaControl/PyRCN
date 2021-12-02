a5bd911b67ed25b1beb371dabf966e53949ae542
renierts <peter.steiner@tu-dresden.de>
2021-12-02 13:13:45 +0100

    Preparation for KM-ESN with attention weights and state transitions.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -232,7 +232,7 @@ except FileNotFoundError:
                                            searches=searches).fit(X_train,
                                                                   y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_km_esn_attention_0_1_rec_0_1"
+         "../sequential_search_speech_timit_km_esn_attention_0_1_rec_-1_1"
          ".joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
 
