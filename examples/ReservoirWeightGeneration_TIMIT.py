1967f630c6463855d57e5da5305f5f5dd34bba2c
renierts <peter.steiner@tu-dresden.de>
2021-12-06 07:41:33 +0100

    Preparation for KM-ESN with attention weights and state transitions.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -219,7 +219,7 @@ searches = [('step1', RandomizedSearchCV, step1_esn_params, kwargs_step1),
             ('step4', RandomizedSearchCV, step4_esn_params, kwargs_step4)]
 
 base_esn = ESNClassifier(input_to_node=input_to_node,
-                         node_to_node=node_to_node
+                         # node_to_node=node_to_node
                          ).set_params(**initially_fixed_params)
 
 try:
@@ -231,7 +231,7 @@ except FileNotFoundError:
                                            searches=searches).fit(X_train,
                                                                   y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_km_esn_rec_0_1"
+         "../sequential_search_speech_timit_km_esn"
          ".joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
 
@@ -244,15 +244,13 @@ for params in ParameterGrid(param_grid):
     kmeans = load("../kmeans_" + str(params["hidden_layer_size"]) + ".joblib")
     w_in = np.divide(kmeans.cluster_centers_,
                      np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
-    w_rec = transition_matrix(kmeans.labels_)
-    estimator.predefined_input_weights = w_in.T
-    estimator.predefined_recurrent_weights = w_rec
+    estimator.input_to_node.predefined_input_weights = w_in.T
     try:
-        cv = load("../speech_timit_km_esn_rec_0_1_"
+        cv = load("../speech_timit_km_esn_"
                   + str(params["hidden_layer_size"]) + ".joblib")
     except FileNotFoundError:
         cv = GridSearchCV(estimator=estimator, param_grid={}, scoring=scoring,
                           n_jobs=5, verbose=10).fit(X=X_train, y=y_train)
-        dump(cv, "../speech_timit_km_esn_rec_0_1_" +
+        dump(cv, "../speech_timit_km_esn_" +
              str(params["hidden_layer_size"]) + ".joblib")
     print(cv.cv_results_)
