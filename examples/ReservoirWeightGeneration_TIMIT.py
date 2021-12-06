70831a144fb25ff5c4c676f5d670189bf2fa6b71
renierts <peter.steiner@tu-dresden.de>
2021-12-06 09:01:33 +0100

    Preparation for KM-ESN with attention weights and state transitions.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -224,14 +224,14 @@ base_esn = ESNClassifier(input_to_node=input_to_node,
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_km_esn_0_1_"
+        "../sequential_search_speech_timit_km_esn_rec_0_1_"
         ".joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(base_esn,
                                            searches=searches).fit(X_train,
                                                                   y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_km_esn_0_1_"
+         "../sequential_search_speech_timit_km_esn_rec_0_1_"
          ".joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
 
@@ -248,11 +248,11 @@ for params in ParameterGrid(param_grid):
     estimator.input_to_node.predefined_input_weights = w_in.T
     estimator.node_to_node.predefined_recurrent_weights = w_rec
     try:
-        cv = load("../speech_timit_km_esn_0_1_"
+        cv = load("../speech_timit_km_esn_rec_0_1_"
                   + str(params["hidden_layer_size"]) + ".joblib")
     except FileNotFoundError:
         cv = GridSearchCV(estimator=estimator, param_grid={}, scoring=scoring,
                           n_jobs=5, verbose=10).fit(X=X_train, y=y_train)
-        dump(cv, "../speech_timit_km_esn_0_1_" +
+        dump(cv, "../speech_timit_km_esn_rec_0_1_" +
              str(params["hidden_layer_size"]) + ".joblib")
     print(cv.cv_results_)
