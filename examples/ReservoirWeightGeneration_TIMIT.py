8776c27cb2acae5b05bbe14fd874df80c169a860
renierts <peter.steiner@tu-dresden.de>
2021-12-08 11:50:55 +0100

    Preparation for KM-ESN with attention weights and state transitions.

--- examples/ReservoirWeightGeneration_TIMIT.py
+++ examples/ReservoirWeightGeneration_TIMIT.py
@@ -165,6 +165,8 @@ w_in = np.divide(kmeans.cluster_centers_,
 input_to_node = PredefinedWeightsInputToNode(
     predefined_input_weights=w_in.T,
 )
+w_rec = transition_matrix(kmeans.labels_)
+node_to_node = PredefinedWeightsNodeToNode(predefined_recurrent_weights=w_rec)
 
 initially_fixed_params = {
     'hidden_layer_size': 50,
@@ -213,20 +215,20 @@ searches = [('step1', RandomizedSearchCV, step1_esn_params, kwargs_step1),
             ('step3', RandomizedSearchCV, step3_esn_params, kwargs_step3),
             ('step4', RandomizedSearchCV, step4_esn_params, kwargs_step4)]
 
-base_esn = ESNClassifier(  # input_to_node=input_to_node,
-                         # node_to_node=node_to_node
+base_esn = ESNClassifier(input_to_node=input_to_node,
+                         node_to_node=node_to_node
                          ).set_params(**initially_fixed_params)
 
 try:
     sequential_search = load(
-        "../sequential_search_speech_timit_km_esn"
+        "../sequential_search_speech_timit_km_esn_rec_0_1"
         ".joblib")
 except FileNotFoundError:
     sequential_search = SequentialSearchCV(base_esn,
                                            searches=searches).fit(X_train,
                                                                   y_train)
     dump(sequential_search,
-         "../sequential_search_speech_timit_km_esn"
+         "../sequential_search_speech_timit_km_esn_rec_0_1"
          ".joblib")
 print(sequential_search.all_best_params_, sequential_search.all_best_score_)
 
@@ -234,7 +236,7 @@ param_grid = {
     'hidden_layer_size': [50, 100, 200, 400, 500, 800, 1000,
                           1600, 2000, 3200, 4000, 6400, 8000, 16000],
 }
-for params in reversed(list(ParameterGrid(param_grid))):
+for params in ParameterGrid(param_grid):
     estimator = clone(sequential_search.best_estimator_).set_params(**params)
     kmeans = load("../kmeans_" + str(params["hidden_layer_size"])
                   + ".joblib")
@@ -242,11 +244,11 @@ for params in reversed(list(ParameterGrid(param_grid))):
                      np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
     estimator.input_to_node.predefined_input_weights = w_in.T
     try:
-        cv = load("../speech_timit_km_esn_"
+        cv = load("../speech_timit_km_esn_rec_0_1_"
                   + str(params["hidden_layer_size"]) + ".joblib")
     except FileNotFoundError:
         cv = GridSearchCV(estimator=estimator, param_grid={}, scoring=scoring,
                           n_jobs=5, verbose=10).fit(X=X_train, y=y_train)
-        dump(cv, "../speech_timit_km_esn_" +
+        dump(cv, "../speech_timit_km_esn_rec_0_1_" +
              str(params["hidden_layer_size"]) + ".joblib")
     print(cv.cv_results_)
