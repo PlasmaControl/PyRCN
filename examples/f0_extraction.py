80c3e8d78c21d22448a8319225c542792fbaaf26
renierts <peter.steiner@tu-dresden.de>
2022-01-04 07:35:24 +0100

    Preparation for KM-ESN with attention weights and state transitions.

--- examples/f0_extraction.py
+++ examples/f0_extraction.py
@@ -8,14 +8,15 @@ from sklearn.utils import shuffle
 from sklearn.utils.fixes import loguniform
 from scipy.stats import uniform
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import (ParameterGrid, GridSearchCV,
-                                     RandomizedSearchCV)
+from sklearn.model_selection import (ParameterGrid, RandomizedSearchCV)
 from sklearn.metrics import make_scorer, zero_one_loss
+from sklearn.cluster import MiniBatchKMeans
 from pyrcn.model_selection import SequentialSearchCV
 from pyrcn.util import FeatureExtractor
 from pyrcn.datasets import fetch_ptdb_tug_dataset
 from pyrcn.echo_state_network import ESNRegressor
 from pyrcn.base.blocks import PredefinedWeightsInputToNode
+import time
 
 
 def create_feature_extraction_pipeline(sr=16000):
@@ -77,20 +78,6 @@ def gpe(y_true, y_pred):
         / len(np.nonzero(y_true)[0])
 
 
-def new_gpe(y_true, y_pred):
-    """
-    Gross pitch error
-    -----------------
-
-    All frames that are considered voiced by both pitch tracker and ground
-    truth, for which the relative pitch error is higher than a certain
-    threshold (20 percent).
-    """
-    idx = np.nonzero(y_true*y_pred)[0]
-    return np.sum(np.abs(1/y_true[idx] - 1/y_pred[idx]) > 1.5e-3) \
-        / len(np.nonzero(y_true)[0])
-
-
 def vde(y_true, y_pred):
     """
     Voicing Decision Error
@@ -119,41 +106,6 @@ def fpe(y_true, y_pred):
         return 100 * np.std(np.log2(y_pred[idx] / y_true[idx]))
 
 
-def mu_fpe(y_true, y_pred):
-    """
-    Fine Pitch Error
-    ----------------
-
-    Standard deviation of the distribution of relative error values (in cents)
-    from the frames that do not have gross pitch errors.
-    """
-    idx_voiced = np.nonzero(y_true * y_pred)[0]
-    idx_correct = np.argwhere(np.abs(1/y_true - 1/y_pred) <= 1.5e-3).ravel()
-    idx = np.intersect1d(idx_voiced, idx_correct)
-    if idx.size == 0:
-        return 0
-    else:
-        return np.mean(np.abs(y_pred[idx] - y_true[idx]))
-
-
-def sigma_fpe(y_true, y_pred):
-    """
-    Fine Pitch Error
-    ----------------
-
-    Standard deviation of the distribution of relative error values (in cents)
-    from the frames that do not have gross pitch errors.
-    """
-    idx_voiced = np.nonzero(y_true * y_pred)[0]
-    idx_correct =\
-        np.argwhere(np.abs(1 / y_true - 1 / y_pred) <= 1.5e-3).ravel()
-    idx = np.intersect1d(idx_voiced, idx_correct)
-    if idx.size == 0:
-        return 0
-    else:
-        return np.std(np.abs(y_pred[idx] - y_true[idx]))
-
-
 def ffe(y_true, y_pred):
     """
     $f_{0}$ Frame Error
@@ -170,7 +122,7 @@ def ffe(y_true, y_pred):
 
 
 def custom_scorer(y_true, y_pred):
-    gross_pitch_error = [None] * len(y_true)
+    gross_pitch_error = np.zeros(shape=(len(y_true),))
     for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
         gross_pitch_error[k] = gpe(y_true=y_t[:, 0]*y_t[:, 1],
                                    y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
@@ -178,6 +130,13 @@ def custom_scorer(y_true, y_pred):
 
 
 gpe_scorer = make_scorer(custom_scorer, greater_is_better=False)
+
+t1 = time.time()
+kmeans = MiniBatchKMeans(n_clusters=50, n_init=200, reassignment_ratio=0,
+                         max_no_improvement=50, init='k-means++', verbose=2,
+                         random_state=0)
+kmeans.fit(X=np.concatenate(np.concatenate((X_train, X_test))))
+print("done in {0}!".format(time.time() - t1))
 
 # Set up a ESN
 # To develop an ESN model for f0 estimation, we need to tune several hyper-
@@ -190,7 +149,7 @@ gpe_scorer = make_scorer(custom_scorer, greater_is_better=False)
 # At last, we initialize an ESNRegressor with the desired output strategy and
 # with the initially fixed parameters.
 
-initially_fixed_params = {'hidden_layer_size': 500,
+initially_fixed_params = {'hidden_layer_size': 50,
                           'k_in': 10,
                           'input_scaling': 0.4,
                           'input_activation': 'identity',
@@ -207,77 +166,61 @@ step1_esn_params = {'input_scaling': uniform(loc=1e-2, scale=1),
                     'spectral_radius': uniform(loc=0, scale=2)}
 
 step2_esn_params = {'leakage': loguniform(1e-5, 1e0)}
-step3_esn_params = {'bias_scaling': np.linspace(0.0, 1.0, 11)}
+step3_esn_params = {'bias_scaling': uniform(loc=0, scale=3)}
 step4_esn_params = {'alpha': loguniform(1e-5, 1e1)}
 
 kwargs_step1 = {'n_iter': 200, 'random_state': 42, 'verbose': 1, 'n_jobs': -1,
                 'scoring': gpe_scorer}
 kwargs_step2 = {'n_iter': 50, 'random_state': 42, 'verbose': 1, 'n_jobs': -1,
                 'scoring': gpe_scorer}
-kwargs_step3 = {'verbose': 1, 'n_jobs': -1, 'scoring': gpe_scorer}
+kwargs_step3 = {'n_iter': 50, 'random_state': 42, 'verbose': 1, 'n_jobs': -1,
+                'scoring': gpe_scorer}
 kwargs_step4 = {'n_iter': 50, 'random_state': 42, 'verbose': 1, 'n_jobs': -1,
                 'scoring': gpe_scorer}
 
 searches = [('step1', RandomizedSearchCV, step1_esn_params, kwargs_step1),
             ('step2', RandomizedSearchCV, step2_esn_params, kwargs_step2),
-            ('step3', GridSearchCV, step3_esn_params, kwargs_step3),
+            ('step3', RandomizedSearchCV, step3_esn_params, kwargs_step3),
             ('step4', RandomizedSearchCV, step4_esn_params, kwargs_step4)]
 
-base_esn = ESNRegressor(**initially_fixed_params)
+w_in = np.divide(kmeans.cluster_centers_,
+                 np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
+
+base_input_to_node = PredefinedWeightsInputToNode(
+    predefined_input_weights=w_in.T, input_scaling=0.4)
+
+base_esn = ESNRegressor(input_to_node=base_input_to_node
+                        ).set_params(**initially_fixed_params)
 
 try:
-    sequential_search = load("f0/sequential_search_f0_mel_km_50.joblib")
+    sequential_search = load("../f0/sequential_search_f0_mel_km_50.joblib")
 except FileNotFoundError:
     print(FileNotFoundError)
     sequential_search = SequentialSearchCV(
         base_esn, searches=searches).fit(X_train, y_train)
-    dump(sequential_search, "f0/sequential_search_f0_mel_km_50.joblib")
+    dump(sequential_search, "../f0/sequential_search_f0_mel_km_50.joblib")
 
 print(sequential_search)
 
-
+"""
 def gpe_scorer(y_true, y_pred):
-    gross_pitch_error = [None] * len(y_true)
+    gross_pitch_error = np.zeros(shape=(len(y_true),))
     for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
         gross_pitch_error[k] = gpe(y_true=y_t[:, 0]*(y_t[:, 1] > 0.5),
                                    y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
     return np.mean(gross_pitch_error)
 
 
-def new_gpe_scorer(y_true, y_pred):
-    gross_pitch_error = [None] * len(y_true)
-    for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
-        gross_pitch_error[k] = new_gpe(y_true=y_t[:, 0]*(y_t[:, 1] > 0.5),
-                                       y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
-    return np.mean(gross_pitch_error)
-
-
 def fpe_scorer(y_true, y_pred):
-    fine_pitch_error = [None] * len(y_true)
+    fine_pitch_error = np.zeros(shape=(len(y_true),))
     for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
         fine_pitch_error[k] = fpe(y_true=y_t[:, 0]*(y_t[:, 1] > 0.5),
                                   y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
     return np.mean(fine_pitch_error)
 
 
-def mu_fpe_scorer(y_true, y_pred):
-    fine_pitch_error = [None] * len(y_true)
-    for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
-        fine_pitch_error[k] = mu_fpe(y_true=y_t[:, 0]*(y_t[:, 1] > 0.5),
-                                     y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
-    return np.mean(fine_pitch_error)
-
-
-def sigma_fpe_scorer(y_true, y_pred):
-    fine_pitch_error = [None] * len(y_true)
-    for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
-        fine_pitch_error[k] = sigma_fpe(y_true=y_t[:, 0]*(y_t[:, 1] > 0.5),
-                                        y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
-    return np.mean(fine_pitch_error)
-
-
 def vde_scorer(y_true, y_pred):
-    voicing_decision_error = [None] * len(y_true)
+    voicing_decision_error = np.zeros(shape=(len(y_true),))
     for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
         voicing_decision_error[k] = vde(y_true=(y_t[:, 1] > 0.5),
                                         y_pred=y_p[:, 1] >= .5)
@@ -285,25 +228,21 @@ def vde_scorer(y_true, y_pred):
 
 
 def ffe_scorer(y_true, y_pred):
-    frame_fault_error = [None] * len(y_true)
+    frame_fault_error = np.zeros(shape=(len(y_true),))
     for k, (y_t, y_p) in enumerate(zip(y_true, y_pred)):
         frame_fault_error[k] = ffe(y_true=y_t[:, 0]*(y_t[:, 1] > 0.5),
                                    y_pred=y_p[:, 0]*(y_p[:, 1] >= .5))
     return np.mean(frame_fault_error)
 
 
-y_pred = load("f0/km_esn_dense_2000_0_0.joblib").predict(X_test)
-gpe_scorer(y_test, y_pred)
-new_gpe_scorer(y_test, y_pred)
-fpe_scorer(y_test, y_pred)
-mu_fpe_scorer(y_test, y_pred)
-sigma_fpe_scorer(y_test, y_pred)
-vde_scorer(y_test, y_pred)
-ffe_scorer(y_test, y_pred)
-
-param_grid = {'hidden_layer_size': [6400]}
+param_grid = {'hidden_layer_size': [50, 100, 200, 400, 500, 800, 1000, 1600],
+              'random_state': [1, 2, 3, 4, 5]}
 for params in ParameterGrid(param_grid):
-    kmeans = load("f0/kmeans_6400.joblib")
+    kmeans = MiniBatchKMeans(n_clusters=params["hidden_layer_size"],
+                             random_state=params["random_state"],
+                             n_init=200, reassignment_ratio=0,
+                             max_no_improvement=50, init='k-means++',
+                             verbose=2)
     w_in = np.divide(kmeans.cluster_centers_,
                      np.linalg.norm(kmeans.cluster_centers_, axis=1)[:, None])
     print(w_in.shape)
@@ -312,5 +251,7 @@ for params in ParameterGrid(param_grid):
     all_params = sequential_search.best_estimator_.get_params()
     all_params["hidden_layer_size"] = params["hidden_layer_size"]
     esn = ESNRegressor(input_to_node=base_input_to_node, **all_params)
-    esn.fit(X_train, y_train, n_jobs=4)
-    dump(esn, "f0/km_esn_dense_6400_4_0.joblib")
+    esn.fit(X_train, y_train)
+    dump(esn, "f0/km_esn_" + str(params["hidden_layer_size"]) + "_" +
+         str(params["random_state"]) + ".joblib")
+"""
